9491cfd24a955800826e6f8ef27b24a66619326e
Luis <lferreiraarg@gmail.com>
2025-11-12 20:01:49 -0300

    Initial commit

---
+++ src/test/java/com/utn/adn/AdnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.adn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
