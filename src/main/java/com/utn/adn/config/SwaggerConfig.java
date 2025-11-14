e3dca112a68de8437cf501b13b7a94311ffea281
Luis <lferreiraarg@gmail.com>
2025-11-13 22:18:56 -0300

    Swagger configurado

---
+++ src/main/java/com/utn/adn/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.utn.adn.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Mutant Detector API")
+                        .version("1.0.0")
+                        .description("Esta API permite analizar secuencias de ADN para determinar si pertenecen a un mutante o a un humano. \n" +
+                                "                        La verificación se realiza mediante un algoritmo que detecta patrones repetitivos en direcciones \n" +
+                                "                        horizontal, vertical y diagonal dentro de una matriz NxN de caracteres (A, T, C, G).")
+                        .contact(new Contact().name("Luis Ferreira").email("lferreiraarg@gmail.com"))
+                );
+    }
+}
